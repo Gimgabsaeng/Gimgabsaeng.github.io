09c9f6e708e2a6cdff8170723d37f839fc2dfaa0
gimforprimelife <gimforprimelife@gmail.com>
2021-05-30 22:04:51 +0900

    fix d day counter bug

--- js/counter.js
+++ js/counter.js
@@ -1,14 +1,19 @@
 (() => {
-    var dday = new Date("June 14, 2021, 0:00:00").getTime();
+    var dday = new Date("May 30, 2021, 20:07:00").getTime()
 
     setInterval(function () {
-        var today = new Date().getTime();
-        var gap = dday - today;
-        var day = Math.ceil(gap / (1000 * 60 * 60 * 24));
-        var hour = Math.ceil((gap % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        var min = Math.ceil((gap % (1000 * 60 * 60)) / (1000 * 60));
-        var sec = Math.ceil((gap % (1000 * 60)) / 1000);
-
-        document.querySelector("#count").innerHTML = "D-" + day + "일 " + hour + "시간 " + min + "분 " + sec + "초";
-    }, 1000);
+        var today = new Date().getTime()
+        var gap = dday - today
+        var day = Math.ceil(gap / (1000 * 60 * 60 * 24)) - 1
+        var hour = Math.ceil((gap % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)) - 1
+        var min = Math.ceil((gap % (1000 * 60 * 60)) / (1000 * 60)) - 1
+        var sec = Math.ceil((gap % (1000 * 60)) / 1000)
+        
+        if (gap >= 0) {
+            document.querySelector("#count").innerHTML = "D-" + day + "일 " + hour + "시간 " + min + "분 " + sec + "초"
+        } else {
+            document.querySelector("#count").innerHTML = null
+        }
+        
+    }, 1000)
 })()
